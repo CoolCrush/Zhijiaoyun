a33b2e2fd62a23cb05e3ffd7bb252a4c5b44eca1
Coolcr <coolcrush@126.com>
2021-07-13 16:09:09 +0800

    完成首页、关于页面的创建

--- app/src/main/java/com/coolcr/zhijiaoyun/ui/MainActivity.java
+++ app/src/main/java/com/coolcr/zhijiaoyun/ui/MainActivity.java
@@ -1,15 +1,28 @@
 package com.coolcr.zhijiaoyun.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import com.coolcr.zhijiaoyun.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.mainFrameLayout)
+    FrameLayout mFrameLayout;
+    @BindView(R.id.mainNavigationView)
+    BottomNavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
---
+++ app/src/main/java/com/coolcr/zhijiaoyun/ui/fragment/AboutFragment.java
@@ -0,0 +1,15 @@
+package com.coolcr.zhijiaoyun.ui.fragment;
+
+import com.coolcr.zhijiaoyun.base.BaseFragment;
+
+/**
+ * Created by CoolCrush
+ * On 2021/7/13
+ * Email CoolCrush@126.com
+ */
+public class AboutFragment extends BaseFragment {
+    @Override
+    protected int getRootViewResId() {
+        return 0;
+    }
+}
